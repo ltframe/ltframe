a391fefa65b67b7df72beecf7d24e48b7696eb05
ltframe <ltplayer@yeah.net>
2019-12-08 15:21:04 +0800

    --

---
+++ LTFRAME_SRC/LTSkin/jsBind.cpp
@@ -0,0 +1,16 @@
+#include <WebCore/config.h>
+#include <JavaScriptCore/JSGlobalObject.h>
+#include <JavaScriptCore/JSFunction.h>
+#include <JavaScriptCore/SourceCode.h>
+#include <JavaScriptCore/Completion.h>
+#include <WebCore/GCController.h>
+#include <WebCore/JSDOMWindowCustom.h>
+#include <WebCore/Page.h>
+#include <WebCore/Frame.h>
+#include <WebCore/Chrome.h>
+#include <WebCore/ChromeClient.h>
+
+void onCreateGlobalObject(JSC::JSGlobalObject* globalObject)
+{
+
+}
